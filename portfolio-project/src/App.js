33fc9be8f3c22ce7da797a3ee7ac76b15eae9675
Ajudiya-Jay <jajudiya@my.centennialcollege.ca>
2025-05-22 13:18:48 -0400

    solved react script versioin error

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,10 +1,30 @@
 
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import React from 'react';
+import Home from './containers/home';
+import About from './containers/about';
+import Portfolio from './containers/portfolio';
+import Contact from './containers/contact';
+import Skills from './containers/skills';
+import Resume from './containers/resume';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* particles js */}
+
+      {/* navbar */}
+
+      {/* main page content */}
+      <Routes>
+        <Route index path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/portfolio" element={<Portfolio/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/skills" element={<Skills/>} />
+        <Route path="/resume" element={<Resume/>} />
+      </Routes>
     </div>
   );
 }
---
+++ portfolio-project/src/containers/contact/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contact= () => {
+  return (
+    <div className="container">
+      <h1>Skills</h1>
+      <p>Here are some of my skills:</p>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>HTML & CSS</li>
+        <li>Python</li>
+      </ul>
+    </div>
+  );
+}
+export default Contact;
