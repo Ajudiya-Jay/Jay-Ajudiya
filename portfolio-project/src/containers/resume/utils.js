a725892d1d39a52626a573ec44327ec2271e65b1
Ajudiya-Jay <jajudiya@my.centennialcollege.ca>
2025-05-23 08:24:30 -0400

    completed resume page

---
+++ portfolio-project/src/containers/resume/utils.js
@@ -0,0 +1,28 @@
+export const data = {
+    experience: [
+        {
+            title: "Software Engineer",
+            company: "Tech Company",
+            location: "City, State",
+            startDate: "Jan 2020",
+            endDate: "Present",
+            description: "Developing and maintaining web applications using React and Node.js."
+        },
+        {
+            title: "Intern",
+            company: "Another Tech Company",
+            location: "City, State",
+            startDate: "Jun 2019",
+            endDate: "Dec 2019",
+            description: "Assisted in the development of internal tools and applications."
+        }
+    ],
+    education: [
+        {
+            degree: "Bachelor of Science in Computer Science",
+            institution: "University Name",
+            location: "City, State",
+            graduationDate: "May 2020"
+        }
+    ],
+}
