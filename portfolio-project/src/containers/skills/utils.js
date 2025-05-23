e620be5a26661efd2955877006ee66528aa48715
Ajudiya-Jay <jajudiya@my.centennialcollege.ca>
2025-05-23 00:18:27 -0400

    Completed skill page

---
+++ portfolio-project/src/containers/skills/utils.js
@@ -0,0 +1,53 @@
+export const skillsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: 90,
+            },
+            {
+                skillName: "CSS",
+                percentage: 85,
+            },
+            {
+                skillName: "JavaScript",
+                percentage: 80,
+            },
+            {
+                skillName: "React",
+                percentage: 90,
+            },
+        ],
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "Node.js",
+                percentage: 80,
+            },
+            {
+                skillName: "Express.js",
+                percentage: 75,
+            },
+            {
+                skillName: "MongoDB",
+                percentage: 70,
+            },
+        ],
+    },
+    {
+        label: "DEVOPS",
+        data: [
+            {
+                skillName: "Docker",
+                percentage: 70,
+            },
+            {
+                skillName: "Kubernetes",
+                percentage: 60,
+            },
+        ],
+    },
+]
