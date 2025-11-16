cc3398aa219d4e6ef9ae85f2eaed1301377edfe5
Ajudiya-Jay <jajudiya@my.centennialcollege.ca>
2025-11-16 13:45:50 -0500

    Fixed resume page

--- portfolio-project/src/containers/resume/utils.js
+++ portfolio-project/src/containers/resume/utils.js
@@ -4,19 +4,19 @@ export const data = {
             title: "Software Developer",
             company: "Sinelogix Technologies",
             location: "Vadodara, India",
-            startDate: "Jul 2022",
+            startDate: "Jul 2021",
             endDate: "Apr 2023",
             description:
-                "Led the development of a full-scale e-commerce project from scratch for a key client. Implemented responsive front-end features using React.js and enhanced backend functionality with PHP and MySQL. Integrated REST and SOAP APIs, payment gateways, and improved site performance through AJAX optimization and Docker-based deployment. Worked with AWS services to enhance scalability and reliability of the platform."
+                "• Achieved a 30% increase in site performance by optimizing caching and query tuning for Magento 2 e-commerce platforms.\n• Successfully migrated 100% of data from Magento 1 to Magento 2 for large-scale projects like Jaypore.com, ensuring minimal downtime.\n• Enhanced third-party extension compatibility, reducing conflicts by 40% through strategic testing and integration.\n• Spearheaded the implementation of REST APIs, improving page load times by 50% for PLP, PDP & Cart functionality."
         },
         {
             title: "Software Developer",
             company: "Rock Technolabs",
             location: "Ahmedabad, India",
-            startDate: "Apr 2021",
+            startDate: "Jan 2020",
             endDate: "Jun 2022",
             description:
-                "Developed and customized Magento extensions using PHP, JavaScript, AJAX, and MySQL to enhance e-commerce platform functionality. Collaborated with the team to improve site performance, fix bugs, and deliver client-requested features that increased overall usability and customer engagement."
+                "• Developed custom Magento 2 extensions, increasing client engagement by 25% with features like Hide Price for Guest Users and Promotional Pricing.\n• Improved code maintainability and application stability by 35% through diagnosing and resolving extension conflicts.\n• Utilized Jira to track and resolve 95% of client-reported issues, enhancing module functionality and client satisfaction.\n• Collaborated with cross-functional teams to deliver updates, achieving a 20% reduction in sprint cycle times."
         }
     ],
     education: [
